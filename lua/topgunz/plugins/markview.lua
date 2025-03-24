3ac58c5a27b9e8f087a306204276a184e2c183b9
Kan Wongsawan <44800560+gunzgo2mars@users.noreply.github.com>
2025-03-24 15:56:30 +0700

    New plugins pack 2 (#5)
    * update: new plugins 2
    
    * update: add docs about installation step.

--- lua/topgunz/plugins/markview.lua
+++ lua/topgunz/plugins/markview.lua
@@ -1,21 +1,32 @@
 return {
-	{
-		"OXY2DEV/markview.nvim",
-		lazy = false,
+	-- {
+	-- 	"OXY2DEV/markview.nvim",
+	-- 	lazy = false,
 
-		-- For blink.cmp's completion
-		-- source
-		-- dependencies = {
-		--     "saghen/blink.cmp"
-		-- },
-	},
+	-- 	-- For blink.cmp's completion
+	-- 	-- source
+	-- 	-- dependencies = {
+	-- 	--     "saghen/blink.cmp"
+	-- 	-- },
+	-- },
+	--	{
+	--		"iamcco/markdown-preview.nvim",
+	--		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	--		build = "cd app && yarn install",
+	--		init = function()
+	--			vim.g.mkdp_filetypes = { "markdown" }
+	--		end,
+	--		ft = { "markdown" },
+	--	},
 	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		build = "cd app && yarn install",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
+		"toppair/peek.nvim",
+		event = { "VeryLazy" },
+		build = "deno task --quiet build:fast",
+		config = function()
+			require("peek").setup()
+			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
 		end,
-		ft = { "markdown" },
 	},
+	{ "kylef/apiblueprint.vim" },
 }
